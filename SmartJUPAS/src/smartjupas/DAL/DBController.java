d90f0bdb4fc50d0b2dcab1a8c5970c39f6ac5647
eddie <ltwu0324@gmail.com>
2016-10-31 17:25:54 +0800

    *Database Controller Class - Done *Little part of ProgrammeBLL

--- SmartJUPAS/src/smartjupas/DAL/DBController.java
+++ SmartJUPAS/src/smartjupas/DAL/DBController.java
@@ -1,5 +1,48 @@
 package smartjupas.DAL;
+import java.sql.*;
+import java.io.*;
 
 public class DBController {
+	private String dbS = "";
+	private String username = "";
+	private String password = "";
+	private static DBController instance = new DBController();
+	
+	private DBController(){
+		
+	}
+	
+	public static DBController getInstance(){
+		return instance;
+	}
+	
+	public void Initalize(String dbS, String username, String password){
+		this.dbS = dbS;
+		this.username= username;
+		this.password = password;
+		System.out.println("Database Connection Initalized!");
+	}
+	
+	public ResultSet FetchData(String sql){
+		try{
+			if(this.dbS=="" || this.username=="" || this.password==""){
+				throw new IOException("Database not yet initialized.");
+			}
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection(this.dbS, this.username, this.password);
+			System.out.println("Fetching data from the Database");
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			if(rs==null)
+				throw new IOException("Cannot read from database.");
+			return rs;
+		} catch(IOException e){
+			System.out.println(e.getMessage());
+			return null;
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+			return null;
+		}
 
+	}
 }
